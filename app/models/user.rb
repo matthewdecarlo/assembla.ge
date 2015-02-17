db5d94d75a975039f8b1b7f11ca9930b5d92c486
Matthew DeCarlo <matthewwho@me.com>
2015-02-17 07:45:03 -0600

    removed excess comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,21 +9,4 @@ class User < ActiveRecord::Base
     has_many :followers, through: :followings, source: :follower
 
     validates_associated :originators, :followers
-
-    # validates :followees, uniqueness: true, scope: :followe
-    # validate  :follower_not_followee?
-    # validate  :followee_not_follower?
-
-    # def follower_not_followee?
-    #   if followers.include?(self)
-    #   errors.add(:followers, "Cannot follow yourself")
-    #   end
-    # end
-
-    # def followee_not_follower?
-    #   if followees.include?(self)
-    #   errors.add(:followers, "Cannot follow yourself")
-    #   end
-    # end
-
 end
