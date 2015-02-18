5e77d2740aa2dd82842cd20cdece8f4c36629e21
Matthew DeCarlo & Miriam Moore <pair+matthewwho.mmdesign007@devbootcamp.com>
2015-02-18 13:31:27 -0600

    posts cruded

--- app/controllers/post.rb
+++ app/controllers/post.rb
@@ -3,9 +3,14 @@
 #############################################################
 before '/users/:user_id/posts/:id' do
 # make sure that the correct user and post is being viewed
-  poster_id = Post.find(params[:id]).poster_id
-  unless  poster_id == params[:user_id].to_i
-    redirect "/users/#{poster_id}/posts/#{params[:id]}"
+  if params[:id] == "new" and current_user.id == params[:user_id].to_i
+    pass
+  else
+    poster_id = Post.find(params[:id]).poster_id
+    p poster_id
+    unless  poster_id == params[:user_id].to_i
+      redirect "/users/#{poster_id}/posts/#{params[:id]}"
+    end
   end
 end
 
@@ -22,10 +27,7 @@ before '/users/:user_id/posts/:id/delete' do
   end
 end
 
-get '/users/:id/posts/new' do
-
-end
-
+#show
 get '/users/:id/posts' do
   @user = User.find(params[:id])
   @posts = @user.posts
@@ -33,12 +35,30 @@ get '/users/:id/posts' do
   erb :'/posts/show'
 end
 
+# create
+get '/users/:id/posts/new' do
+  erb :'/posts/new'
+end
+
+post '/users/:id/posts' do
+  @post = Post.new(poster_id: current_user.id, content: params[:content])
+
+  if @post.save
+    p @post.id
+    redirect "/users/:id/posts/#{@post.id}"
+  else
+    redirect "/users/:id/posts/new"
+  end
+end
+
+#show individual
 get '/users/:user_id/posts/:id' do
   @user = User.find(params[:user_id])
   @post = Post.find(params[:id])
   erb :'/posts/post'
 end
 
+#edit
 get '/users/:user_id/posts/:id/edit' do
   @post = Post.find(params[:id])
   erb :'posts/edit'
@@ -54,8 +74,9 @@ put '/users/:user_id/posts/:id' do
   end
 end
 
+#delete
 delete '/users/:user_id/posts/:id' do
   post = Post.find(params[:id])
   post.destroy
-  redirect '/users/:user_id/posts'
+  redirect "/users/#{current_user.id}/posts"
 end
